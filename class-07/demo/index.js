adeb12808d8133e107fdcec0393c85e789abf875
JacobKnaack <jacobknck@gmail.com>
2020-06-16 12:49:46 -0700

    removed practice test files

--- class-07/demo/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const server = require('./lib/server.js');
-
-server.listen(3000, () => console.log('Server up on :: 3000'));
--- class-07/demo/lib/logger.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = (req, res, next) => {
-  console.log('REQUEST', req.method, req.path);
-  next();
-}
